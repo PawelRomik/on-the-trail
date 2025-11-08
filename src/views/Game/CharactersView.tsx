9e8d3e047c92902f74cb9bf3c41de1d334672b0c
Paweł Romik <leoneczek15@gmail.com>
2025-11-08 11:17:15 +0100

    feat: added navbar

--- src/views/Game/CharactersView.tsx
+++ src/views/Game/CharactersView.tsx
@@ -8,10 +8,23 @@ type CharactersViewProps = {
 
 export default function CharactersView({ characters, onSelect }: CharactersViewProps) {
 	return (
-		<div className='w-[80%] m-auto h-[800px] flex gap-4 items-center justify-center'>
-			{characters.map((char) => (
-				<Character key={char.id} character={char} onClick={() => onSelect(char)} />
-			))}
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='w-[80%] m-auto h-[800px] flex gap-4 items-center justify-center'>
+				{characters.map((char) => (
+					<Character key={char.id} character={char} onClick={() => onSelect(char)} />
+				))}
+			</div>
+			<nav className='w-[200px] h-full bg-black flex flex-col p-5 text-6xl gap-5 items-center justify-center'>
+				<button className='bg-zinc-800 border-2 hover:cursor-pointer flex-1 border-zinc-700 w-full text-white'>
+					<i className='ri-book-2-fill'></i>
+				</button>
+				<button className='bg-zinc-800 border-2 hover:cursor-pointer flex-1 border-zinc-700 w-full text-white'>
+					<i className='ri-auction-fill'></i>
+				</button>
+				<button className='bg-zinc-800 border-2 hover:cursor-pointer flex-1 border-zinc-700 w-full text-white'>
+					<i className='ri-settings-2-fill'></i>
+				</button>
+			</nav>
 		</div>
 	);
 }
