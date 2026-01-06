241e82be0d0fb621356c3019902398411dac36ac
Paweł Romik <leoneczek15@gmail.com>
2026-01-06 14:58:39 +0100

    style: Update settings styles

--- src/components/settings/SettingsActions.tsx
+++ src/components/settings/SettingsActions.tsx
@@ -23,12 +23,21 @@ export default function SettingsActions({ onBack, onReset }: SettingsActionsProp
 
 	return (
 		<div className='flex justify-between pt-4'>
-			<button className='px-4 py-2 cursor-pointer bg-zinc-600 rounded-md hover:bg-zinc-700' onClick={backToMenu}>
-				{t("settings.backToMenu")}
+			<button
+				className='px-4 py-2 cursor-pointer  bg-[rgba(0,0,0,0.5)]  border-b-2 border-red-800  rounded-xl transition hover:brightness-200 hover:-translate-y-0.5'
+				onClick={backToMenu}
+			>
+				<span className='gap-1 flex'>
+					<i className='ri-door-open-line text-red-800'></i>
+					{t("settings.backToMenu")}
+				</span>
 			</button>
 
-			<button className='px-4 py-2 cursor-pointer bg-red-500 text-white rounded-md hover:bg-red-600' onClick={resetGame}>
-				{t("settings.resetGame")}
+			<button className='px-4 py-2 cursor-pointer bg-[rgba(255,0,0,0.5)]  border-b-2 border-red-800  rounded-xl hover:brightness-200 hover:-translate-y-0.5 ' onClick={resetGame}>
+				<span className='gap-1 flex'>
+					<i className='ri-reset-right-line text-white'></i>
+					{t("settings.resetGame")}
+				</span>
 			</button>
 		</div>
 	);
--- src/components/settings/VolumeSlider.tsx
+++ src/components/settings/VolumeSlider.tsx
@@ -6,12 +6,27 @@ type VolumeSliderProps = {
 };
 
 export default function VolumeSlider({ id, label, value, onChange }: VolumeSliderProps) {
+	const percent = value;
+
+	const backgroundStyle = {
+		background: `linear-gradient(to right, #991b1b 0%, #991b1b ${percent}%, #111 ${percent}%, #000 100%)`
+	};
+
 	return (
 		<div className='flex flex-col'>
 			<label htmlFor={id} className='mb-2 font-medium'>
 				{label}
 			</label>
-			<input id={id} type='range' min={0} max={100} value={value} onChange={(e) => onChange(Number(e.target.value))} className='cursor-grab w-full' />
+			<input
+				id={id}
+				type='range'
+				min={0}
+				max={100}
+				value={value}
+				onChange={(e) => onChange(Number(e.target.value))}
+				className='w-full cursor-grab h-2 rounded-lg appearance-none'
+				style={backgroundStyle}
+			/>
 		</div>
 	);
 }
