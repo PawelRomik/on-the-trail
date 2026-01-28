9d012c64052078b1d8698e4be6ca555bfca1e365
Paweł Romik <leoneczek15@gmail.com>
2026-01-28 09:49:48 +0100

    style: Update book styles

--- src/components/notepad/Stickers.tsx
+++ src/components/notepad/Stickers.tsx
@@ -2,61 +2,59 @@ import { useTranslation } from "react-i18next";
 import { useCharactersContext } from "../../utils/context/character-context/useCharacterContext";
 import playSound from "../../utils/misc/playSound";
 import { useSettings } from "../../utils/context/settings-context/useSettings";
+import detective from "../../assets/character/detective.png";
+import all from "../../assets/ui/all.png";
 
 type StickersProps = {
+	showNotes: boolean;
+	filterCharacter: string | null | undefined;
 	setShowNotes: (val: boolean) => void;
 	setFilterCharacter: (val: string | null | undefined) => void;
 };
 
-export default function Stickers({ setShowNotes, setFilterCharacter }: StickersProps) {
+const stickerClass = (active: boolean) =>
+	`h-12 min-w-20 px-3 cursor-pointer text-white font-bold italic rounded-t-lg -translate-y-5 shadow-md ml-3 flex items-center justify-center ${
+		active ? "bg-purple-700 h-16" : "bg-purple-800 "
+	}`;
+
+export default function Stickers({ showNotes, filterCharacter, setShowNotes, setFilterCharacter }: StickersProps) {
 	const { characters } = useCharactersContext();
 	const { t } = useTranslation();
 	const { voiceVolume } = useSettings();
 
-	const showNotes = () => {
+	const getCharacterImage = (id: number) => `../assets/character/ch${id}.png`;
+
+	const showNotesHandler = () => {
 		playSound("page_flip", voiceVolume);
 		setShowNotes(true);
 		setFilterCharacter(null);
 	};
 
-	const showAll = () => {
+	const showAllHandler = () => {
 		playSound("page_flip", voiceVolume);
 		setFilterCharacter(null);
 		setShowNotes(false);
 	};
 
-	const showChar = (title: string) => {
+	const showCharHandler = (title: string) => {
 		playSound("page_flip", voiceVolume);
 		setFilterCharacter(title);
 		setShowNotes(false);
 	};
 
 	return (
-		<div className='w-full absolute h-[60px] -top-9 z-0 flex items-end gap-0'>
-			<button
-				className='h-12 min-w-20 px-3 cursor-pointer bg-yellow-300 text-white font-bold italic rounded-t-lg -translate-y-5 shadow-md ml-3 flex items-center justify-center'
-				onClick={showNotes}
-			>
-				{t("notes.notes")}
+		<div className='w-full absolute h-[60px] -top-9 z-0 flex items-end'>
+			<button onClick={showNotesHandler} className={stickerClass(showNotes)}>
+				<img src={detective} alt={t("notes.notes")} className='h-10 w-10 object-contain rounded-full' />
 			</button>
 
-			<button
-				className='h-12 min-w-20 px-3 cursor-pointer bg-blue-500 text-white font-bold italic rounded-t-lg -translate-y-5 shadow-md ml-3 flex items-center justify-center'
-				onClick={showAll}
-			>
-				{t("notes.all")}
+			<button onClick={showAllHandler} className={stickerClass(!showNotes && !filterCharacter)}>
+				<img src={all} alt={t("notes.all")} className='h-10 w-10 object-contain rounded-full' />
 			</button>
 
 			{characters.map((ch, index) => (
-				<button
-					key={ch.name}
-					style={{ backgroundColor: ch.color }}
-					className={`h-12 min-w-20 px-3 cursor-pointer text-white font-bold italic rounded-t-lg -translate-y-5 shadow-md ml-3 hover:bg-yellow-200 z-${
-						10 + index
-					} flex items-center justify-center`}
-					onClick={() => showChar(ch.title)}
-				>
-					{t(`characters.ch${ch.id}.title`)}
+				<button key={ch.name} onClick={() => showCharHandler(ch.title)} className={`${stickerClass(filterCharacter === ch.title)} z-${10 + index}`}>
+					<img src={getCharacterImage(ch.id)} alt={t(`characters.ch${ch.id}.title`)} className='h-10 w-10 object-contain rounded-full' />
 				</button>
 			))}
 		</div>
