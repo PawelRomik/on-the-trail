844730955bc5a759997613990c26f31cb05a8624
Paweł Romik <leoneczek15@gmail.com>
2026-02-23 12:11:58 +0100

    style: Update assets

--- src/types/CharacterType.ts
+++ src/types/CharacterType.ts
@@ -1,10 +1,12 @@
 export type CharacterType = {
 	id: number;
 	name?: string;
+	setName?: string;
 	title: string;
 	age: number | number[];
 	stressMeter: number;
 	gender: boolean;
+	setGender?: boolean;
 	traits: {
 		buffs?: string[];
 		nerfs?: string[];
