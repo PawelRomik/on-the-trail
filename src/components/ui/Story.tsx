be4bc0797016d29857debacdf46e6beaee1e077a
Paweł Romik <leoneczek15@gmail.com>
2026-08-14 21:19:05 +0200

    fix: update image URL and styles

--- src/components/ui/Story.tsx
+++ src/components/ui/Story.tsx
@@ -7,7 +7,7 @@ export default function Story() {
 	const currentLanguage = i18n.language as "pl" | "en";
 
 	return (
-		<div className='text-white text-center bg-[rgba(0,0,0,0.8)] px-[5%] md:px-[10%] mb-5 py-3 text-sm gap-3 flex flex-col'>
+		<div className='text-white text-center bg-[rgba(0,0,0,0.8)] px-[5%] md:px-[10%]  py-3 text-sm gap-3 flex flex-col'>
 			<h1 className='uppercase text-2xl md:text-4xl font-bold'>{t(`location.${location}`)}</h1>
 			<h3 className='text-zinc-400 text-[9px] md:text-lg italic'>{intro?.[currentLanguage]}</h3>
 		</div>
